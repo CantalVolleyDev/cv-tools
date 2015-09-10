69351212c5d84be03432fceae32bb130b8492fc9
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-10 22:30:28 +0200

    [New] Ajout liste des actions

---
+++ src/main/java/com/jtouzy/cv/tools/Commands.java
@@ -0,0 +1,7 @@
+package com.jtouzy.cv.tools;
+
+public class Commands {
+	public static final String TOOL_OPTION = "t";
+	public static final String DEPLOY_API_OPTION = "webapi";
+	public static final String DEPLOY_WEBAPP_OPTION = "webapp";
+}
